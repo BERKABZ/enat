265b9f09573085c6754dbcfeff6cb0cb4a1c7b6d
BERKABZ <150333064+BERKABZ@users.noreply.github.com>
2023-11-22 02:49:47 +0800

    Add files via upload

---
+++ ANOTAJ/src/main/java/com/mycompany/anotaj/ANOTAJ.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.anotaj;
+
+/**
+ *
+ * @author Edward
+ */
+public class ANOTAJ {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
